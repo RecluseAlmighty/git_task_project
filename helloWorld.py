6e3f7ac1810ad31b18b7a57027a98966c33588fe
RecluseAlmighty <aaronmorris1295@hotmail.com>
2023-12-02 23:49:59 +0000

    comments now added to code for clarity

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,2 @@
-user_input = (input("What is your name : ")).upper()
-print(user_input)
+user_input = (input("What is your name : ")).upper() # receives user input converting all inputs to uppercase
+print(user_input) # outputting user input to user
